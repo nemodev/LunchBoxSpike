acc0d4bb02b003634dcb11def0b760fcd8a548bb
Bruce Yang <whitestone0819@gmail.com>
2013-06-04 08:00:24 -0400

    move bootstrap folders under 'bootstrap'

--- app.js
+++ app.js
@@ -7,14 +7,13 @@ var server = http.createServer(function(request, response){
 		response.writeHead(200, {'Content-Type':'text/html'});
 		response.end(data);
 	});
-}).listen(52273, function() {
+}).listen(process.env.PORT, function() {
 	
 });
 
 var io = socketio.listen(server);
 io.sockets.on('connection', function(socket){
 	socket.on('record', function(data){
-		debugger;
 		console.log(data);
 	});
 });
